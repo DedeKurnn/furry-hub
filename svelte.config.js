0238faea08ebb675cbee18f3cdaf9b83709d7a01
Dede Kurniawan <90181030@bri.co.id>
2026-03-12 09:32:57 +0700

    initial commit

---
+++ svelte.config.js
@@ -0,0 +1,14 @@
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-cloudflare';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: { adapter: adapter() },
+	vitePlugin: {
+		dynamicCompileOptions: ({ filename }) => ({ runes: !filename.includes('node_modules') })
+	},
+	preprocess: [mdsvex()],
+	extensions: ['.svelte', '.svx']
+};
+
+export default config;
